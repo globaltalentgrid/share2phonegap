342ce465d4cedb241d1197b08606249ff37e3956
globaltalentgrid <globaltalentgrid@gmail.com>
2015-10-03 15:32:59 +0530

    change v6

--- src/android/nl/xservices/plugins/ShareMyApp.java
+++ src/android/nl/xservices/plugins/ShareMyApp.java
@@ -42,68 +42,35 @@ public class ShareMyApp extends CordovaPlugin {
   @Override
   public void onNewIntent(Intent intent) {
       // Get intent, action and MIME type
-    Intent intent = getIntent();
+    //Intent intent = getIntent();
     String action = intent.getAction();
     String type = intent.getType();
 
     if (Intent.ACTION_SEND.equals(action) && type != null) {
         if ("text/plain".equals(type)) {
-            handleSendText(intent); // Handle text being sent
+            	String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+		    	if (sharedText != null) {webView.loadUrl("javascript:handleNewIntent('sharedtext', '" + sharedText + "');");}
         } else if (type.startsWith("image/")) {
-            handleSendImage(intent); // Handle single image being sent
+                Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+    			if (imageUri != null) {webView.loadUrl("javascript:handleNewIntent('singleimage', '" + imageUri + "');");}
         } else if (type.startsWith("video/")) {
-            handleSendVideo(intent); // Handle single video being sent
+                Uri videoUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+    			if (videoUri != null) {webView.loadUrl("javascript:handleNewIntent('singlevideo', '" + videoUri + "');");}
         }
     } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
         if (type.startsWith("image/")) {
-            handleSendMultipleImages(intent); // Handle multiple images being sent
+                ArrayList<Uri> imageUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+			    if (imageUris != null) {webView.loadUrl("javascript:handleNewIntent('multipleimages', '" + imageUris + "');");}
         } else if (type.startsWith("video/")) {
-            handleSendMultipleVideos(intent); // Handle multiple videos being sent
+			    ArrayList<Uri> videoUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+			    if (videoUris != null) {webView.loadUrl("javascript:handleNewIntent('multiplevideos', '" + videoUris + "');");}
         }
     } else {
         // Handle other intents, such as being started from the home screen
     }
    }
 
-  private static void handleSendText(Intent intent) {
-    String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
-    if (sharedText != null) {
-        // Update UI to reflect text being shared
-        webView.loadUrl("javascript:handleNewIntent('sharedtext', '" + sharedText + "');");
-    }
-}
 
-  private static void handleSendImage(Intent intent) {
-    Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-    if (imageUri != null) {
-        // Update UI to reflect image being shared
-                webView.loadUrl("javascript:handleNewIntent('singleimage', '" + imageUri + "');");
-    }
-}
-
-  private static void handleSendMultipleImages(Intent intent) {
-    ArrayList<Uri> imageUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
-    if (imageUris != null) {
-        // Update UI to reflect multiple images being shared
-                webView.loadUrl("javascript:handleNewIntent('multipleimages', '" + imageUris + "');");
-    }
-}
-
-  private static void handleSendVideo(Intent intent) {
-    Uri videoUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-    if (videoUri != null) {
-        // Update UI to reflect video being shared
-                webView.loadUrl("javascript:handleNewIntent('singlevideo', '" + videoUri + "');");
-    }
-}
-
-  private static void handleSendMultipleVideos(Intent intent) {
-    ArrayList<Uri> videoUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
-    if (videoUris != null) {
-        // Update UI to reflect multiple videos being shared
-                webView.loadUrl("javascript:handleNewIntent('multiplevideos', '" + videoUris + "');");
-    }
-}
 
 
   // Taken from commons StringEscapeUtils
